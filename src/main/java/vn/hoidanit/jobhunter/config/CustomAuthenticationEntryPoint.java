a94ed5dc7abd5d07575083e3f35e6a2f6416d0c1
phamthang271003 <phamtranquangthang30@gmail.com>
2024-09-01 20:59:48 +0700

    fixbugJwt

--- src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -13,6 +13,8 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.lang.StackWalker.Option;
+import java.util.Optional;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -33,7 +35,9 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
+        String errorMessage = Optional.ofNullable(authException.getCause()).map(Throwable::getMessage)
+                .orElse(authException.getMessage());
+        res.setError(errorMessage);
         res.setMessage("Token không hợp lệ (hết hạn, không đúng định dạng, hoặc không truyền JWT ở header)...");
 
         mapper.writeValue(response.getWriter(), res);
